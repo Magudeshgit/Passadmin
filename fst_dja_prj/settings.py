dc288c642e1672b1938dc7e2a42e824dd1cc85e6
Magudesh <74949557+Magudeshgit@users.noreply.github.com>
2022-03-06 14:49:58 +0530

    Update settings.py

--- fst_dja_prj/settings.py
+++ fst_dja_prj/settings.py
@@ -127,7 +127,7 @@ LOGIN_URL = 'register.views.loginpg'
 
 STATIC_URL = '/static/'
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/Images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/Images')
 # Default primary key field type
