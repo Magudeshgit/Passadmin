541dc19196bd612f4f63b2fcd82cfbd404ec8179
Magudesh <74949557+Magudeshgit@users.noreply.github.com>
2022-03-12 22:20:03 +0530

    Delete my_app directory

--- my_app/passwordform.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm, widgets
-from .models import passmanager
-
-class passer(ModelForm):
-	class Meta:
-		model = passmanager
-		fields = ['Website','Username','password']
